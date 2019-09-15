510a4a42bdfd340f22aa99dc9e334ffb4c4dd998
lucaslimadevs <lucaslima.devs@gmail.com>
2019-09-15 20:49:05 -0300

    atualizando material aula 29

---
+++ formacao-angular/projetos/calculadora/src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
+import { ProviderAst } from '@angular/compiler';
+import { CalculadoraService } from './services'
+
+@NgModule({
+  declarations: [CalculadoraComponent],
+  imports: [
+    CommonModule
+  ],
+  exports : [
+    CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
+  ]
+})
+export class CalculadoraModule { }
