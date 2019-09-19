6c4dd24698db83e85dc06ebf72d577fb1f2d33ea
lucaslimadevs <lucaslima.devs@gmail.com>
2019-09-19 17:48:55 -0300

    Aula 32

--- formacao-angular/projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
+++ formacao-angular/projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,9 @@
+/** 
+*Serviço responsável por executar as operações da calculadora.
+*@author Lucas Santos
+*@since 1.0.0
+*/
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +11,22 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /* Define as constantes utilizadas para identificar as
+  operações de calculo */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number; // armezena o resultado da operação
+
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      
+    }
+  }
 }
